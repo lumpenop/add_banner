f192cfae515ef1a8356925a0e708b6a24845f765
lumpenop <swk5373@gmail.com>
2021-03-29 12:05:53 +0900

    add menu slider

---
+++ menu.js
@@ -0,0 +1,11 @@
+
+function over(){
+    snb = document.querySelector(".snb_area")
+    snb.className = "snb_area navi";
+  
+}
+
+function out(){
+    snb = document.querySelector(".snb_area")
+    snb.className = "snb_area navi2";
+}
