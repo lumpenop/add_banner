c26f67cd00e236bc46161bd422e5b003d4def1fd
lumpenop <swk5373@gmail.com>
2021-03-30 09:29:44 +0900

    slider 완성

--- slider.js
+++ slider.js
@@ -11,7 +11,31 @@ function slider(){
         document.querySelector('.interview ul').className = 'move2';
         }, 1000);
      
-  
 }
 
-setInterval(slider, 2000);
+rollin = setInterval(slider, 2000);
+
+
+function left(){
+    clearInterval(rollin);
+    slider()
+    /* rollin = setInterval(slider, 2000); */
+}
+
+function right(){
+    clearInterval(rollin);
+    li = document.querySelectorAll('.interview li');
+    newLi=document.createElement('li');
+    newLi.innerHTML = li[3].innerHTML;
+
+    document.querySelector('.interview ul').prepend(newLi);
+    document.querySelector('.interview ul').removeAttribute('class');
+    document.querySelector('.interview ul').className = 'move_right';
+
+
+    setTimeout(function(){
+        document.querySelector('.interview ul').className = 'move_right2';
+        document.querySelector('.interview ul').removeChild(li[3]);
+        }, 100);
+    rollin = setInterval(slider, 2000);
+}
